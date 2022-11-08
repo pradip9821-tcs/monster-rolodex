707714528155b71ac6bfbc7911d288fcff43a568
pradip-tcs <100670828+pradip9821-tcs@users.noreply.github.com>
2022-11-08 10:01:42 +0530

    Add more code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monster: [],
+      search: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((monster) => {
+        this.setState({ monster});
+      });
+  }
+
+  render() {
+    const { monster, search } = this.state;
+    const filteredMonster = monster.filter(monster =>
+      monster.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+    return (
+      <div className='App'>
+        <h1>જંગલી માનવ</h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={e => this.setState({ search: e.target.value })}
+        />
+        <CardList monster={filteredMonster} />
+      </div>
+    );
+  }
 }
 
 export default App;
